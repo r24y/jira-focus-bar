675c685baf47fddd83cf7cead1312fbaed0cb973
Ryan Kennnedy <ryan@r24y.com>
2016-07-05 12:47:50 -0400

    Initial commit

---
+++ spec/jira-focus-bar-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import JiraFocusBarView from '../lib/jira-focus-bar-view';
+
+describe('JiraFocusBarView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
